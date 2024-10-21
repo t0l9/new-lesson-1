407489349c13a4a0df877ec722a5954927cafce9
t0l9 <t0l4ik.kas1@gmail.com>
2024-10-21 13:55:28 +0500

    add

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -3,6 +3,9 @@ import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
@@ -17,8 +20,30 @@ public class LoginTest {
                 .shouldHave(Condition.text("Войти"));
         $("[name=email]").setValue("t0l4ik.kas1@gmail.com");
         $("[name=password]").setValue("pa$$w0rd");
-
         //$(".form-control.form-field-email").setValue("t0l4ik.kas1@gmail.com");
 
     }
+
+    @Test
+    void UnSuccessfullLoginTest(){
+        Configuration.holdBrowserOpen = true;
+
+        open("https://school.qa.guru/cms/system/login?required=true");
+        $(".xdget-loginUserForm")
+                .shouldHave(Condition.text("Войти"));
+        $("[name=email]").setValue("t0l4ik.kas1@gmail.com");
+        $("[name=password]").setValue("pa$$w0rd");
+        //$(".form-control.form-field-email").setValue("t0l4ik.kas1@gmail.com");
+
+    }
+
+    @Test
+    void shouldOpen() {
+        String date = "14.02.1996";
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        LocalDate.parse(date, formatter);
+    }
+    
+
+
 }
